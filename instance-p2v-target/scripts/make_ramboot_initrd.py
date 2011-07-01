bf01f9a77277fac95c246b3edc3cacb8f36b8c02
Ben Lipton <benlipton@google.com>
2011-07-01 16:06:10 -0400

    Add make_ramboot_initrd.py and unit tests
    Added a script that generates an initrd suitable for running an OS
    entirely from RAM, to be used as a target for physical-to-virtual
    transfers.
    
    Signed-off-by: Ben Lipton <benlipton@google.com>
    [iustin@google.com: converted to epydoc formatting and other fixes]
    Signed-off-by: Iustin Pop <iustin@google.com>
    Reviewed-by: Iustin Pop <iustin@google.com>

---
+++ instance-p2v-target/scripts/make_ramboot_initrd.py
@@ -0,0 +1,293 @@
+#!/usr/bin/python
+#
+# Copyright 2011 Google Inc. All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
+"""Construct initrd capable of booting operating system entirely from RAM.
+
+This script is meant to be run on a ganeti node to construct an initrd
+that will be used to run the bootstrap operating system for a
+physical-to-virtual (P2V) transfer.
+
+This script uses the program mkinitramfs from initramfs-tools to
+generate an initrd that is compatible with the currently running
+kernel but copies the contents of $ROOT into a tmpfs and mounts the
+tmpfs instead of the real disk.  This allows the operating system to
+do whatever it likes to the disk (partition, format, copy files)
+without interfering with its own operation.
+
+Caveats:
+  - The operating system booted with this initrd must not try to mount
+    the disk itself in its init scripts. This usually means replacing
+    the root filesystem listed in /etc/fstab with the line::
+        tmpfs         /         tmpfs       defaults    0     0
+  - Because the disk is not mounted, rebooting the os will erase any
+    changes that have been made to the root filesystem like installing
+    packages or network configuration. If possible, these should be done
+    by the OS template.
+  - The boot process still depends on the contents of $ROOT. Thus, if
+    the P2V process repartitions the disk and then fails, the bootstrap
+    OS will need to be reinstalled before it can be booted again.
+
+"""
+
+import optparse
+import os
+import os.path
+import platform
+import shutil
+import subprocess
+import sys
+import tempfile
+
+MOVETORAM_SCRIPT = """#!/bin/sh
+
+# movetoram: copies the contents of \$rootmnt to a tmpfs volume, then
+# replaces the mounted disk with the tmpfs so that the system will run
+# entirely from RAM
+
+case $1 in
+  prereqs)
+    exit 0 ;;
+esac
+
+mkdir /tmproot
+mount -t tmpfs -o size=500M,mode=0777 tmpfs /tmproot
+cp -r ${rootmnt}/* /tmproot
+umount ${rootmnt}
+mount -o move /tmproot ${rootmnt}
+
+exit 0
+"""
+
+
+class Error(Exception):
+  pass
+
+
+def ParseOptions(argv):
+  """Parse any options on the command line.
+
+  @param argv: the argv the program received on the command line
+
+  @returns: options dictionary
+
+  """
+  parser = optparse.OptionParser()
+  parser.add_option("-n", "--file-name", dest="file_name",
+                    help="file name for initrd [initrd.img-<VERSION>-ramboot]",
+                    default=None)
+  parser.add_option("-d", "--config-dir", dest="conf_dir",
+                    help=("original initramfs-tools configuration directory "
+                          "[%default]"),
+                    default="/etc/initramfs-tools")
+  parser.add_option("-b", "--boot-dir", dest="boot_dir",
+                    help="install generated initrd to BOOT_DIR [%default]",
+                    default="/boot")
+  parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
+                    default=False, help="output status messages [%default]")
+  parser.add_option("-k", "--keep-temp", action="store_true", dest="keep_temp",
+                    default=False, help="keep temporary config dir")
+  parser.add_option("-f", "--force", action="store_true", dest="overwrite",
+                    default=False, help="overwrite existing file")
+  parser.add_option("-V", "--version", dest="version",
+                    help=("generate initrd for kernel named VERSION "
+                          "[currently running kernel]"),
+                    default=None)
+
+  (options, _) = parser.parse_args(argv[1:])
+
+  if not options.version:
+    options.version = platform.release()
+
+  if not options.file_name:
+    if options.version:
+      options.file_name = "initrd.img-%s-ramboot" % options.version
+    else:
+      options.file_name = "initrd-modified.img"
+
+  return options
+
+
+def CreateTempDir(conf_dir):
+  """Creates temporary configuration dir.
+
+  Securely creates a temporary directory and copies the files from
+  conf_dir into it. This directory will be used to the configure the
+  creation of the initrd.
+
+  @param conf_dir: path to the original initramfs-tools configuration dir
+
+  @return: path to the temporary directory
+
+  """
+  temp_dir = tempfile.mkdtemp()
+  new_conf_dir = os.path.join(temp_dir, "initramfs-tools")
+  try:
+    shutil.copytree(conf_dir, new_conf_dir)
+  except (IOError, OSError):
+    CleanUp(temp_dir)
+    raise Error("Error copying config files. Please ensure you have read"
+                " access to the %s directory." % conf_dir)
+  return temp_dir, new_conf_dir
+
+
+def CleanUp(temp_dir):
+  """Try to clean up the temporary files.
+
+  Removes the temporary files, but doesn't raise an Error if it
+  doesn't work, because that would cause an infinite loop. Just gives
+  up and informs the user.
+
+  @param temp_dir: directory to remove
+
+  """
+  if temp_dir and os.path.isdir(temp_dir):
+    try:
+      shutil.rmtree(temp_dir)
+    except (OSError, IOError):
+      print ("Unexpected error removing temp directory %s. "
+             "Not cleaning up.") % temp_dir
+      sys.exit(1)
+
+
+def AddScript(conf_dir):
+  """Add the script to be run at boot time.
+
+  Adds a script that copies the root filesystem from disk to memory,
+  putting it in a location where it will be picked up and run by the
+  initrd
+
+  @param conf_dir: temporary configuration directory
+
+  @raises Error: layout of conf_dir is not as expected
+
+  """
+  script_dir = os.path.join(conf_dir, "scripts", "local-bottom")
+  if not os.path.isdir(script_dir):
+    raise Error("Script does not understand config directory layout. "
+                "It should contain a scripts/local-bottom directory.")
+
+  movetoram_name = os.path.join(script_dir, "movetoram")
+  movetoram_file = open(movetoram_name, "w")
+  movetoram_file.write(MOVETORAM_SCRIPT)
+  movetoram_file.close()
+  os.chmod(movetoram_name, 0755)
+
+
+def BuildInitrd(temp_dir, conf_dir, file_name, version):
+  """Build the initrd using mkinitramfs.
+
+  Runs the program mkinitramfs to create an initrd for the specified
+  kernel version
+
+  @param temp_dir: temporary directory to hold initrd
+  @param conf_dir: temporary configuration directory with added script
+  @param file_name: what to call the initrd file
+  @param version: the kernel version to use
+
+  @return: the location of the generated initrd in the temp_dir
+
+  @raises Error: call to mkinitramfs failed
+
+  """
+  temp_out = os.path.join(temp_dir, file_name)
+  ret = subprocess.call(["mkinitramfs", "-d", conf_dir,
+                         "-o", temp_out, version])
+  if ret != 0:
+    raise Error("Failed building initramfs")
+
+  return temp_out
+
+
+def InstallInitrd(temp_out, install_dir, file_name, overwrite=False):
+  """Install the initrd to the specified location.
+
+  Moves the initrd to the specified install_dir, and sets the
+  permissions securely.
+
+
+  @param temp_out: full path to the generated initrd
+  @param install_dir: where to install it to
+  @param file_name: filename of the initrd
+  @param overwrite: whether to overwrite one that's there already
+
+  @raises Error: Install directory not writeable or already contains initrd
+
+  """
+  dest_filename = os.path.join(install_dir, file_name)
+  if os.path.exists(dest_filename) and not overwrite:
+    raise Error("A file named %s already exists in the directory %s. If you "
+                "wish to overwrite it, please pass the -f "
+                "option" % (file_name, install_dir))
+  try:
+    shutil.copy(temp_out, install_dir)
+    os.chmod(dest_filename, 0644)
+  except (IOError, OSError):
+    raise Error("Error installing the new initrd. Please make sure you can"
+                " write to the selected boot directory (%s) or select a new"
+                " one with the -b option." % install_dir)
+
+
+def main(argv):
+  options = ParseOptions(argv)
+  conf_dir = options.conf_dir
+  version = options.version
+  file_name = options.file_name
+  install_dir = options.boot_dir
+  verbose = options.verbose
+  temp_dir = None
+  dest_filename = os.path.join(install_dir, file_name)
+
+  try:
+    # Catch some errors early
+    if not os.access(install_dir, os.W_OK):
+      raise Error("Can not write to boot directory. Not generating initrd.")
+    if os.path.exists(dest_filename) and not options.overwrite:
+      raise Error("A file named %s already exists in the directory %s."
+                  " If you wish to overwrite it, please pass the -f option" %
+                  (file_name, install_dir))
+
+    if verbose:
+      print "Configuring..."
+    temp_dir, new_conf_dir = CreateTempDir(conf_dir)
+    if verbose:
+      print "Adding script..."
+    AddScript(new_conf_dir)
+
+    if verbose:
+      print "Building..."
+    temp_out = BuildInitrd(temp_dir, new_conf_dir, file_name, version)
+
+    if verbose:
+      print "Installing..."
+    InstallInitrd(temp_out, install_dir, file_name, options.overwrite)
+
+  except Error, e:
+    print e
+    sys.exit(1)
+  finally:
+    if verbose:
+      print "Cleaning up..."
+
+    if options.keep_temp:
+      print temp_dir
+    else:
+      CleanUp(temp_dir)
+
+if __name__ == "__main__":
+  main(sys.argv)
---
+++ instance-p2v-target/test/make_ramboot_initrd_test.py
@@ -0,0 +1,393 @@
+#!/usr/bin/python
+#
+# Copyright 2011 Google Inc. All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
+"""Tests for scripts.make_ramboot_initrd."""
+
+import os
+import platform
+import shutil
+import tempfile
+import unittest
+import mox
+
+import scripts.make_ramboot_initrd as mkinitrd
+
+class MakeRambootInitrdTest(unittest.TestCase):
+  SRCDIR = os.environ.get("SRCDIR", ".")
+  MOVETORAM = os.path.join(SRCDIR, "test", "testdata", "movetoram")
+
+  def setUp(self):
+    self.mox = mox.Mox()
+
+    self.conf_dir = tempfile.mkdtemp()
+    self.install_dir = tempfile.mkdtemp()
+    self.test_filename = "test"
+    self.test_file_contents = "test"
+    self.test_filepath = os.path.join(self.conf_dir, self.test_filename)
+    self.dirs_to_remove = [self.conf_dir, self.install_dir]
+    os.makedirs(os.path.join(self.conf_dir, "scripts", "local-bottom"))
+
+    test_file = open(self.test_filepath, "w")
+    test_file.write(self.test_file_contents)
+    test_file.close()
+    os.chmod(self.conf_dir, 0755)
+    os.chmod(self.test_filepath, 0644)
+
+    self.temp_dir, self.new_conf_dir = mkinitrd.CreateTempDir(self.conf_dir)
+    self.dirs_to_remove.append(self.temp_dir)
+
+  def tearDown(self):
+    self.mox.UnsetStubs()
+    self.mox.ResetAll()
+    for d in self.dirs_to_remove:
+      if d and os.path.isdir(d):
+        shutil.rmtree(d)
+
+  def testCreateTempDirCopiesFiles(self):
+    self.assertTrue(os.path.isdir(self.temp_dir))
+    self.assertTrue(os.path.isdir(self.new_conf_dir))
+
+    filename = os.path.join(self.new_conf_dir, self.test_filename)
+    self.assertTrue(os.path.isfile(filename))
+
+    f = open(filename, "r")
+    contents = f.read(5)
+    self.assertEqual(self.test_file_contents, contents)
+    f.close()
+
+  def testCreateTempDirPermissions(self):
+    # Ensure that only the user running the script can see into the directory
+    # from which the initrd is being built
+
+    tdmode = os.stat(self.temp_dir).st_mode & 0777
+    self.assertEqual(0700, tdmode)
+
+    ncdmode = os.stat(self.new_conf_dir).st_mode & 0777
+    self.assertEqual(0755, ncdmode)
+
+    filename = os.path.join(self.new_conf_dir, self.test_filename)
+    filemode = os.stat(filename).st_mode & 0777
+    self.assertEqual(0644, filemode)
+
+  def testCreateTempDirHandlesPermissionDenied(self):
+    self.mox.StubOutWithMock(mkinitrd.tempfile, "mkdtemp")
+    self.mox.StubOutWithMock(mkinitrd.shutil, "copytree")
+
+    tempdirname = "/tmp/12345"
+    confdirname = os.path.join(tempdirname, "initramfs-tools")
+    mkinitrd.tempfile.mkdtemp().AndReturn(tempdirname)
+    mkinitrd.shutil.copytree(self.conf_dir,
+                             confdirname).AndRaise(OSError("Permission denied"))
+
+    self.mox.ReplayAll()
+
+    # Make sure we fail if user can't read conf_dir
+    self.assertRaises(mkinitrd.Error, mkinitrd.CreateTempDir, self.conf_dir)
+
+    self.mox.VerifyAll()
+
+  def testInstallInitrdHandlesPermissionDenied(self):
+    self.mox.StubOutWithMock(mkinitrd.shutil, "copy")
+    mkinitrd.shutil.copy(self.test_filepath,
+                         self.install_dir).AndRaise(OSError("Denied"))
+
+    self.mox.ReplayAll()
+
+    # Make sure we fail if user can't write boot_dir
+    try:
+      self.assertRaises(mkinitrd.Error, mkinitrd.InstallInitrd,
+                        self.test_filepath,  # file to copy
+                        self.install_dir,  # where to put it
+                        self.test_filename)  # name of file
+    except (OSError, IOError):
+      self.fail()
+
+    self.mox.VerifyAll()
+
+  def testInstallInitrdHandlesExistingFile(self):
+    # Make sure that we ask for confirmation if boot_dir/file_name exists
+    dest_filename = os.path.join(self.install_dir, self.test_filename)
+    open(dest_filename, "a").close()  # Create file
+    self.assertRaises(mkinitrd.Error, mkinitrd.InstallInitrd,
+                      self.test_filepath,  # file to copy (would be the initrd)
+                      self.install_dir,  # where to put it
+                      self.test_filename)  # name of file
+
+  def testInstallInitrdInstallsFile(self):
+    mkinitrd.InstallInitrd(self.test_filepath,  # file to copy
+                           self.install_dir,  # where to put it
+                           self.test_filename)  # name of file
+    dest_filename = os.path.join(self.install_dir, self.test_filename)
+    self.assertTrue(os.path.isfile(dest_filename))
+    filemode = os.stat(dest_filename).st_mode & 0777
+    self.assertEqual(0644, filemode)
+
+  def testCleanUpRemovesFiles(self):
+    mkinitrd.CleanUp(self.temp_dir)
+    self.assertFalse(os.path.exists(self.temp_dir))
+
+  def testCleanUpHandlesMissingDir(self):
+    mkinitrd.CleanUp(self.temp_dir)
+    try:
+      mkinitrd.CleanUp(self.temp_dir)  # should be no error even doing it twice
+    except (OSError, IOError):
+      self.fail()
+
+  def testCleanUpQuitsCleanlyOnError(self):
+    self.mox.StubOutWithMock(mkinitrd.shutil, "rmtree")
+    mkinitrd.shutil.rmtree(self.temp_dir).AndRaise(OSError("Permission denied"))
+
+    self.mox.ReplayAll()
+
+    self.assertRaises(SystemExit, mkinitrd.CleanUp, self.temp_dir)
+
+    self.mox.VerifyAll()
+
+  def testAddScriptCreatesCorrectScript(self):
+    dest_filename = os.path.join(self.new_conf_dir, "scripts",
+                                 "local-bottom", "movetoram")
+
+    try:
+      mkinitrd.AddScript(self.new_conf_dir)
+    except mkinitrd.Error:
+      self.fail()  # should not produce error on normal conf dir
+
+    self.assertTrue(os.path.isfile(dest_filename))
+    src_file = open(self.MOVETORAM, "r")
+    dst_file = open(dest_filename, "r")
+    src_contents = src_file.read(500)
+    dst_contents = dst_file.read(500)
+    self.assertEqual(src_contents, dst_contents)
+
+  def testAddScriptDetectsImproperConfigDir(self):
+    os.rmdir(os.path.join(self.new_conf_dir, "scripts", "local-bottom"))
+
+    self.assertRaises(mkinitrd.Error, mkinitrd.AddScript, self.new_conf_dir)
+
+  def testBuildInitrdRunsCommand(self):
+    self.mox.StubOutWithMock(mkinitrd.subprocess, "call")
+    test_name = "testinitrd"
+    test_tmp = os.path.join("/tmp", "test_makeinitrd")
+    test_out = os.path.join(test_tmp, test_name)
+    test_conf = os.path.join(test_tmp, "conf")
+    test_version = "2.6-test"
+    mkinitrd.subprocess.call(["mkinitramfs", "-d", test_conf,
+                              "-o", test_out, test_version]).AndReturn(0)
+    self.mox.ReplayAll()
+
+    try:
+      mkinitrd.BuildInitrd(test_tmp, test_conf, test_name, test_version)
+    except mkinitrd.Error:
+      self.fail()
+    finally:
+      self.mox.VerifyAll()
+
+  def testBuildInitrdRaisesErrorOnFailure(self):
+    self.mox.StubOutWithMock(mkinitrd.subprocess, "call")
+    test_name = "testinitrd"
+    test_tmp = os.path.join("/tmp", "test_makeinitrd")
+    test_out = os.path.join(test_tmp, test_name)
+    test_conf = os.path.join(test_tmp, "conf")
+    test_version = "2.6-test"
+    mkinitrd.subprocess.call(["mkinitramfs", "-d", test_conf,
+                              "-o", test_out, test_version]).AndReturn(1)
+    self.mox.ReplayAll()
+
+    self.assertRaises(mkinitrd.Error, mkinitrd.BuildInitrd, test_tmp,
+                      test_conf, test_name, test_version)
+
+    self.mox.VerifyAll()
+
+  def testParseArgsHandlesArgsCorrectly(self):
+    old_conf_dir = "/etc/new-initramfs-tools"
+    boot_dir = "/boot/subdir"
+    file_name = "testinitrd.img"
+    version = "2.6-test"
+    argv = ["make_ramboot_initrd_test.py", "-d", old_conf_dir, "-v",
+            "-n", file_name, "-b", boot_dir, "-V", version]
+
+    options = mkinitrd.ParseOptions(argv)
+
+    self.assertEqual(file_name, options.file_name)
+    self.assertEqual(boot_dir, options.boot_dir)
+    self.assertTrue(options.verbose)
+    self.assertEqual(old_conf_dir, options.conf_dir)
+    self.assertFalse(options.keep_temp)
+    self.assertFalse(options.overwrite)
+    self.assertEqual(version, options.version)
+
+  def testParseArgsHandlesNoArgsCorrectly(self):
+    argv = ["make_ramboot_initrd_test.py"]
+
+    options = mkinitrd.ParseOptions(argv)
+
+    expected_version = platform.release()
+    self.assertEqual(expected_version, options.version)
+    self.assertEqual("initrd.img-%s-ramboot" % expected_version,
+                     options.file_name)
+    self.assertEqual("/boot", options.boot_dir)
+    self.assertFalse(options.verbose)
+    self.assertEqual("/etc/initramfs-tools", options.conf_dir)
+    self.assertFalse(options.keep_temp)
+    self.assertFalse(options.overwrite)
+
+  def testParseArgsHandlesWeirdScriptName(self):
+    # make sure it doesn't get thrown off by being called with a strange name
+    argv = ["-v"]  # called with -v as script name
+
+    options = mkinitrd.ParseOptions(argv)
+    self.assertFalse(options.verbose)
+
+  def testMainHandlesOptions(self):
+    old_conf_dir = "/etc/new-initramfs-tools"
+    temp_dir = "/tmp/abcde"
+    new_conf_dir = os.path.join(temp_dir, "confdir")
+    temp_out = os.path.join(temp_dir, "initrd")
+    boot_dir = self.install_dir
+    file_name = "testinitrd.img"
+    version = "2.6-test"
+
+    self.mox.StubOutWithMock(mkinitrd, "CreateTempDir")
+    self.mox.StubOutWithMock(mkinitrd, "AddScript")
+    self.mox.StubOutWithMock(mkinitrd, "BuildInitrd")
+    self.mox.StubOutWithMock(mkinitrd, "InstallInitrd")
+    self.mox.StubOutWithMock(mkinitrd, "CleanUp")
+
+    mkinitrd.CreateTempDir(old_conf_dir).AndReturn((temp_dir, new_conf_dir))
+    mkinitrd.AddScript(new_conf_dir)
+    mkinitrd.BuildInitrd(temp_dir, new_conf_dir, file_name,
+                         version).AndReturn(temp_out)
+    mkinitrd.InstallInitrd(temp_out, boot_dir, file_name, False)
+    mkinitrd.CleanUp(temp_dir)
+
+    self.mox.ReplayAll()
+
+    argv = ["make_ramboot_initrd_test.py", "-d", old_conf_dir, "-v",
+            "-n", file_name, "-b", boot_dir, "-V", version]
+
+    mkinitrd.main(argv)
+
+    self.mox.VerifyAll()
+
+  def testMainHandlesNoOptions(self):
+    old_conf_dir = "/etc/initramfs-tools"
+    temp_dir = "/tmp/abcde"
+    new_conf_dir = os.path.join(temp_dir, "confdir")
+    temp_out = os.path.join(temp_dir, "initrd")
+    boot_dir = self.install_dir  # specify this because it must be writable
+    version = platform.release()
+    file_name = "initrd.img-%s-ramboot" % version
+
+    self.mox.StubOutWithMock(mkinitrd, "CreateTempDir")
+    self.mox.StubOutWithMock(mkinitrd, "AddScript")
+    self.mox.StubOutWithMock(mkinitrd, "BuildInitrd")
+    self.mox.StubOutWithMock(mkinitrd, "InstallInitrd")
+    self.mox.StubOutWithMock(mkinitrd, "CleanUp")
+
+    mkinitrd.CreateTempDir(old_conf_dir).AndReturn((temp_dir, new_conf_dir))
+    mkinitrd.AddScript(new_conf_dir)
+    mkinitrd.BuildInitrd(temp_dir, new_conf_dir, file_name,
+                         version).AndReturn(temp_out)
+    mkinitrd.InstallInitrd(temp_out, boot_dir, file_name, False)
+    mkinitrd.CleanUp(temp_dir)
+
+    self.mox.ReplayAll()
+
+    argv = ["make_ramboot_initrd_test.py", "-b", boot_dir]
+
+    mkinitrd.main(argv)
+
+    self.mox.VerifyAll()
+
+  def testMainHandlesError(self):
+    old_conf_dir = "/etc/initramfs-tools"
+    temp_dir = "/tmp/abcde"
+    new_conf_dir = os.path.join(temp_dir, "confdir")
+    boot_dir = self.install_dir
+    version = platform.release()
+    file_name = "initrd.img-%s-ramboot" % version
+
+    self.mox.StubOutWithMock(mkinitrd, "CreateTempDir")
+    self.mox.StubOutWithMock(mkinitrd, "AddScript")
+    self.mox.StubOutWithMock(mkinitrd, "BuildInitrd")
+    self.mox.StubOutWithMock(mkinitrd, "InstallInitrd")
+    self.mox.StubOutWithMock(mkinitrd, "CleanUp")
+
+    mkinitrd.CreateTempDir(old_conf_dir).AndReturn((temp_dir, new_conf_dir))
+    mkinitrd.AddScript(new_conf_dir)
+    mkinitrd.BuildInitrd(temp_dir, new_conf_dir, file_name,
+                         version).AndRaise(mkinitrd.Error("test!"))
+    mkinitrd.CleanUp(temp_dir)
+
+    self.mox.ReplayAll()
+
+    argv = ["make_ramboot_initrd_test.py", "-b", boot_dir]
+
+    self.assertRaises(SystemExit, mkinitrd.main, argv)
+
+    self.mox.VerifyAll()
+
+  def testMainExitsEarlyIfInstallDirUnwritable(self):
+    # none of these should be called, because there's no point in doing
+    # the work if you're going to discard it later
+    self.mox.StubOutWithMock(mkinitrd, "CreateTempDir")
+    self.mox.StubOutWithMock(mkinitrd, "AddScript")
+    self.mox.StubOutWithMock(mkinitrd, "BuildInitrd")
+    self.mox.StubOutWithMock(mkinitrd, "InstallInitrd")
+    self.mox.StubOutWithMock(mkinitrd, "CleanUp")
+
+    self.mox.StubOutWithMock(mkinitrd.os, "access")
+
+    mkinitrd.os.access(self.install_dir, mkinitrd.os.W_OK).AndReturn(False)
+    mkinitrd.CleanUp(None)
+
+    self.mox.ReplayAll()
+
+    argv = ["make_ramboot_initrd_test.py", "-b", self.install_dir]
+
+    self.assertRaises(SystemExit, mkinitrd.main, argv)
+
+    self.mox.VerifyAll()
+
+  def testMainExitsEarlyIfFileExists(self):
+    # none of these should be called, because there's no point in doing
+    # the work if you're going to discard it later
+    self.mox.StubOutWithMock(mkinitrd, "CreateTempDir")
+    self.mox.StubOutWithMock(mkinitrd, "AddScript")
+    self.mox.StubOutWithMock(mkinitrd, "BuildInitrd")
+    self.mox.StubOutWithMock(mkinitrd, "InstallInitrd")
+
+    self.mox.StubOutWithMock(mkinitrd, "CleanUp")
+    mkinitrd.CleanUp(None)
+
+    self.mox.ReplayAll()
+
+    dest_filename = os.path.join(self.install_dir, self.test_filename)
+    open(dest_filename, "a").close()  # Create file
+
+    argv = ["make_ramboot_initrd_test.py", "-b", self.install_dir,
+            "-n", self.test_filename]
+
+    self.assertRaises(SystemExit, mkinitrd.main, argv)
+
+    self.mox.VerifyAll()
+
+
+if __name__ == "__main__":
+  unittest.main()
